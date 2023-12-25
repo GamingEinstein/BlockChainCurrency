b013eabff4ee6ec0d006ca58f9952207b8333e0d
GamingEinstein <gamingeinstein1@gmail.com>
2023-12-25 05:18:36 -0500

    Added Recipes

--- src/main/java/net/gamingeinstein/blockchaincurrency/BlockChainCurrency.java
+++ src/main/java/net/gamingeinstein/blockchaincurrency/BlockChainCurrency.java
@@ -4,7 +4,6 @@ import com.mojang.logging.LogUtils;
 import net.gamingeinstein.blockchaincurrency.block.ModBlocks;
 import net.gamingeinstein.blockchaincurrency.item.ModCreativeModeTabs;
 import net.gamingeinstein.blockchaincurrency.item.ModItems;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -17,13 +16,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(BlockChainCurrency.MOD_ID)
 public class BlockChainCurrency {
 
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "blockchain_currency";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public BlockChainCurrency() {
@@ -44,19 +40,15 @@ public class BlockChainCurrency {
 
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
